02829a510f0c3c07e0000dd421bd6b902225a97e
Yui Matsuura <teshi04@gmail.com>
2021-01-01 03:31:23 +0900

    :sparkles: Implement barcode scanning

--- app/src/main/java/jp/tsur/booksearchscan2/ScanFragment.kt
+++ app/src/main/java/jp/tsur/booksearchscan2/ScanFragment.kt
@@ -3,12 +3,14 @@ package jp.tsur.booksearchscan2
 import android.os.Bundle
 import android.view.View
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import com.google.common.util.concurrent.ListenableFuture
+import jp.tsur.booksearchscan2.camera.ImageAnalyzer
 import jp.tsur.booksearchscan2.databinding.FragmentScanBinding
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -19,11 +21,13 @@ class ScanFragment : Fragment(R.layout.fragment_scan) {
 
     private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
     private lateinit var cameraExecutor: ExecutorService
+    private lateinit var analyzerExecutor: ExecutorService
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentScanBinding.bind(view)
         cameraExecutor = Executors.newSingleThreadExecutor()
+        analyzerExecutor = Executors.newSingleThreadExecutor()
 
         setUpCamera()
     }
@@ -50,8 +54,20 @@ class ScanFragment : Fragment(R.layout.fragment_scan) {
             .requireLensFacing(CameraSelector.LENS_FACING_BACK)
             .build()
 
+        val analyzer = ImageAnalyzer { barcode ->
+            val value = barcode.rawValue
+            // TODO
+        }
+
+        val imageAnalysis = ImageAnalysis.Builder()
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+            .apply {
+                setAnalyzer(analyzerExecutor, analyzer)
+            }
+
         try {
-            cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+            cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview, imageAnalysis)
         } catch (e: Exception) {
             // TODO
         }
